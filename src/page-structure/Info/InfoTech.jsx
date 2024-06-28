38eddb0669e2132809c568a2e8282835e20f32c4
Dogukan Kahraman <dkahraman08@gmail.com>
2024-06-27 21:49:48 -0700

    Feat: Update package.json and package-lock.json to include framer-motion v11.2.12, Add animations for some of the visual objects

--- src/page-structure/Info/InfoTech.jsx
+++ src/page-structure/Info/InfoTech.jsx
@@ -12,11 +12,24 @@ import sql from "../../assets/logo/Sql.svg";
 import mongodb from "../../assets/logo/mongodb.svg";
 import redux from "../../assets/logo/redux.svg";
 import firebase from "../../assets/logo/firebase.svg";
-
+import { motion, useAnimation, useInView } from "framer-motion";
 import "react-tooltip/dist/react-tooltip.css";
 import { Tooltip } from "react-tooltip";
+import { useEffect, useRef } from "react";
 
 function InfoTech() {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    } else {
+      mainControls.start("hidden");
+    }
+  }, [isInView]);
+
   const skills = [
     { name: "C#", img: cs, id: "csharp" },
     { name: "Python", img: py, id: "python" },
@@ -33,18 +46,32 @@ function InfoTech() {
     { name: "Redux", img: redux, id: "redux" },
     { name: "Firebase", img: firebase, id: "firebase" },
   ];
+
   return (
     <div className="w-full flex justify-center items-center mt-12 flex-col">
       <p className="text-center text-4xl font-bold text-green-600">Skills</p>
-      <ul className="grid grid-cols-2 sm:grid-cols-3 sm:gap-16 gap-8 mt-8 justify-items-center lg:grid-cols-4 lg:gap-16">
+      <ul
+        ref={ref}
+        className="grid grid-cols-2 sm:grid-cols-3 sm:gap-16 gap-8 mt-8 justify-items-center lg:grid-cols-4 lg:gap-16"
+      >
         {skills.map((skill, index) => (
           <li key={index}>
-            <img
-              id={skill.id}
-              src={skill.img}
-              alt={skill.name}
-              className="w-16 h-16 sm:w-20 sm:h-20 object-contain"
-            />
+            <motion.div
+              variants={{
+                hidden: { opacity: 0, scale: 0.5, y: -100 },
+                visible: { opacity: 1, scale: 1, y: 0 },
+              }}
+              initial="hidden"
+              animate={mainControls}
+              transition={{ delay: 0.2 + index * 0.1 }}
+            >
+              <img
+                id={skill.id}
+                src={skill.img}
+                alt={skill.name}
+                className="w-16 h-16 sm:w-20 sm:h-20 object-contain"
+              />
+            </motion.div>
             <Tooltip anchorSelect={"#" + skill.id} content={skill.name} />
           </li>
         ))}
