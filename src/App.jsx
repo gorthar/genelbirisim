29568139dff82fe0c0eb690724544ebb9aea50b6
Dogukan Kahraman <dkahraman08@gmail.com>
2024-08-12 16:58:20 -0700

    chore: Remove unnecessary console.log statement in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import ReactGA from "react-ga4";
 
 function App() {
   ReactGA.initialize(import.meta.env.VITE_ANALYTICS_KEY);
-  console.log(import.meta.env.VITE_ANALYTICS_KEY);
   return (
     <>
       <NavBar />
